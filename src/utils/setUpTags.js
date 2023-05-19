7ce560286894f47326babf167af7197b9cbb547d
miha11-hash <d_mihaela@msn.com>
2023-05-19 16:17:28 +0300

    helper function - sort tags

--- src/utils/setUpTags.js
+++ src/utils/setUpTags.js
@@ -1,6 +1,22 @@
 const setUpTags = recipes => {
-    console.log("hello from utils")
-    return recipes
+    let allTags={};
+    recipes.forEach((recipe) => {
+        recipe.content.tags.forEach((tag) => {
+            if(allTags[tag]) {
+                allTags[tag]=allTags[tag]+1
+            } else {
+                allTags[tag]=1
+            }
+        })
+    })
+    console.log(allTags);
+    const sortTags=Object.entries(allTags).sort((a, b) => {
+        const [firstTag]=a;
+        const [secondTag]=b;
+        return firstTag.localeCompare(secondTag);
+    })
+    console.log(sortTags);
+    return sortTags
 }
 
 export default setUpTags
