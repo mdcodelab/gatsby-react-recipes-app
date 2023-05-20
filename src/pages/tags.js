48c4d474c7cbc35cbd3bbff9d7b84bb1d7e8cf1f
miha11-hash <d_mihaela@msn.com>
2023-05-21 00:18:35 +0300

    Tags page

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,20 +1,42 @@
-import React from 'react';
-import Layout from '../components/Layout';
-import styled from "styled-components"
+import React from "react";
+import Layout from "../components/Layout";
+import styled from "styled-components";
+import { graphql, Link, useStaticQuery } from "gatsby";
+import setUpTags from "../utils/setUpTags";
 
+export const query = graphql`
+  query {
+    allContentfulRecipe {
+      nodes {
+        content {
+          tags
+        }
+      }
+    }
+  }
+`;
+console.log(query);
+const Tags = () => {
+  const { allContentfulRecipe } = useStaticQuery(query);
+  const sortTags = setUpTags(allContentfulRecipe.nodes);
 
-const tags = () => {
   return (
     <Layout>
-        <Wrapper>
-            <h2>Tags page</h2>
-        </Wrapper>
+      <main className="page">
+        <section className="tags-page">
+          {sortTags.map((tag, index) => {
+            const [text, value] = tag;
+            return (
+              <Link key={index} className="tag" to={`/${text}`}>
+                  <p>{text}</p>
+                  <p>{value} recipes</p>
+              </Link>
+            );
+          })}
+        </section>
+      </main>
     </Layout>
   );
-}
+};
 
-const Wrapper = styled.div`
-
-`;
-
-export default tags;
+export default Tags;
