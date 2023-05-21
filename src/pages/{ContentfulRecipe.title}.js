55377de35d9dac5c8277d178b0e5f7427843268f
miha11-hash <d_mihaela@msn.com>
2023-05-21 15:28:09 +0300

    RecipeTemplate query

--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -1,12 +1,43 @@
 import React from "react";
+import { graphql} from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import {BsClockHistory, BsClock, BsPeople} from "react-icons/bs";
+import { useStaticQuery } from "gatsby";
 
-function RecipeTemplate(prop) {
-  console.log(prop);
+function RecipeTemplate({data}) {
+  console.log(data);
+  const{title, cookTime, prepTime, servings, description: {description}, content, image}=data.contentfulRecipe;
+  const pathToImage=getImage(image);
+  const {ingredients, instructions, tags, tools}=content
+  
   return (
-    <div>
-      <h4>{prop.params.title}</h4>
-    </div>
+    <main className="page">
+      <div className="recipe-page">
+        <h2>{title}</h2>
+      </div>
+    </main>
   );
 }
+
+export const query = graphql`
+  query MyQuery($title: String) {
+    contentfulRecipe(title: { eq: $title }) {
+      title
+      cookTime
+      prepTime
+      description {description}
+      servings
+      content {
+        ingredients
+        instructions
+        tags
+        tools
+      }
+      image {
+        gatsbyImageData
+      }
+    }
+  }
+`;
 
 export default RecipeTemplate;
