18b765b11754cf3e23d48b990b2b2c0bc4014e9a
miha11-hash <d_mihaela@msn.com>
2023-06-01 21:57:23 +0300

    error page style

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import Layout from "../components/Layout";
 import {Link} from "gatsby";
-import SEO from "../components/SEO"
+import SEO from "../components/SEO";
+import styled from "styled-components";
 
 const Error = () => {
   return (
     <Layout>
     <SEO title="Error"></SEO>
-      <main className="error-page">
+      <Wrapper className="error-page">
         <section>
           <h1>404</h1>
           <h3>Page not found</h3>
           <Link to="/">Back Home</Link>
         </section>
-      </main>
+      </Wrapper>
     </Layout>
   );
 }
+
+const Wrapper = styled.div `
+height: calc()(1--vh - 10rem);
+h1, h3 {
+  text-align: center;
+}
+`;
+
+
 
 
 export default Error;
